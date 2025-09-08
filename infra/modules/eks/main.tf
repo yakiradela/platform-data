2a936f23600f0951167d7946f36ce2a4ef80062b
Yakir Adela <yakirade@gmail.com>
2025-09-08 14:22:28 +0300

    Update main.tf

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -10,7 +10,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "DEV-EKS"
+  cluster_name    = "DEV-EKS-DATA"
   cluster_version = "1.28"
 
   vpc_id     = var.vpc_id
